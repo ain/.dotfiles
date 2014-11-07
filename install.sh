8498d720a9a347024a271293c72c32110a2e3d7d
Ain Tohvri <at@interactive-pioneers.de>
2014-11-07 12:03:34 +0100

    Add PressAndHold option idea

--- install.sh
+++ install.sh
@@ -4,4 +4,14 @@ defaults write -g ApplePressAndHoldEnabled -bool true
 # defaults read ~/Library/Preferences/com.apple.HIToolbox.plist AppleSelectedInputSources | egrep -w 'KeyboardLayout Name' | sed -E 's/^.+ = \"?([^\"]+)\"?;$/\\1/'
 sudo mv /System/Library/Input\ Methods/PressAndHold.app/Contents/Resources/Keyboard-et.plist ~/Downloads/Keyboard-et.backup.plist
 sudo ln -s ~/.dotfiles/Keyboard-et.plist /System/Library/Input\ Methods/PressAndHold.app/Contents/Resources/Keyboard-et.plist
+
+# Disable Apple's PressAndHold on Terminal (optional)
+while getopts disable-press-and-hold name
+do
+  case $name in
+    disable-press-and-hold) echo "Disabling PressAndHold in Terminali...";; defaults write com.apple.Terminal ApplePressAndHoldEnabled -bool false;; echo "PressAndHold disabled!";;
+  esac
+done
+
+
 echo "Complete."
