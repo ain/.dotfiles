a84a0c76fb3b71179037d2aacbde84773118bfe4
Ain Tohvri <at@interactive-pioneers.de>
2015-03-10 10:46:12 +0100

    Add setting Git excludesfile in installation

--- install.sh
+++ install.sh
@@ -2,10 +2,6 @@ echo "Installing dotfiles..."
 
 PWD=`pwd`
 
-echo "  installing global Git ignore configuration"
-unlink ~/.gitignore
-ln -s $PWD/.gitignore ~/.gitignore
-
 echo "  installing Bash profile"
 unlink ~/.bash_profile
 ln -s $PWD/.bash_profile ~/.bash_profile
@@ -13,5 +9,10 @@ ln -s $PWD/.bash_profile ~/.bash_profile
 echo "  installing tmux configuration"
 unlink ~/.tmux.conf
 ln -s $PWD/.tmux.conf ~/.tmux.conf
+
+echo "  installing global Git ignore configuration"
+unlink ~/.gitignore
+ln -s $PWD/.gitignore ~/.gitignore
+git config --global core.excludesfile ~/.gitignore
 
 echo "Installation complete!"
